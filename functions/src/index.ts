ebf63d627f0ed20d6ec9b52ee8a6ec0bef5e1960
t.mato <t.mato@i3design.co.jp>
2019-08-10 18:24:57 +0900

    create sample scheduler

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,32 @@
 import * as functions from 'firebase-functions';
 
+import * as admin from 'firebase-admin';
+
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
 // export const helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+admin.initializeApp();
+
+export const addMessage = functions.https.onRequest(async (req, res) => {
+    const original = req.query.text;
+    const snapshot = await admin.database().ref('/messages').push({original: original});
+    res.redirect(303, snapshot.ref.toString());
+});
+
+export const makeUppercase = functions.database.ref('/messages/{pushId}/original').onCreate((snapshot, context) => {
+    const original = snapshot.val();
+    console.log('Uppercasing', context.params.pushId, original);
+    const uppercase = original.toUpperCase();
+    if (snapshot.ref.parent) {
+        return snapshot.ref.parent.child('uppercase').set(uppercase);
+    }
+    return null;
+})
+
+export const scheduledFunction = functions.pubsub.schedule('every 1 minutes').onRun((context) => {
+    console.log('This will be run every 1 minutes!');
+})
