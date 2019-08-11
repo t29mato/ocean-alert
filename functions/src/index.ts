0da551252ac2ffe1c6cf8d67c2682169916f7a24
t.mato <t.mato@i3design.co.jp>
2019-08-11 13:41:34 +0900

    簡易版完成 pushはSlack利用

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -9,58 +9,77 @@ admin.initializeApp();
 export const crawlingDivingSpot = functions.pubsub.schedule('every 1 minutes').onRun(async (context) => {
     // 最新のデータを取得する
     await admin.database().ref(constants.spots.iwa.dbPath).limitToLast(1).once('value')
-        .then((snapshot) => {
-            const options = {
-                method: 'GET',
-                uri: constants.spots.iwa.uri,
-                encoding: null,
-            };
-            request(options, (error, response, body) => {
-                if (error) {
-                    console.warn(error);
+    .then((snapshot) => {
+        const options = {
+            method: 'GET',
+            uri: constants.spots.iwa.uri,
+            encoding: null,
+        };
+        request(options, (error, response, body) => {
+            if (error) {
+                console.warn(error);
+                return null;
+            }
+
+            // 文字化け対策のDecode処理
+            const buf = Buffer.from(body, 'binary');
+            const retStr = iconv.decode(buf, constants.spots.iwa.charset);
+
+            // 更新トリガーに変更がなければ処理終了
+            const resultUpdateTrigger = retStr.match(constants.spots.iwa.regexp.updateTrigger);
+            const updateTrigger = resultUpdateTrigger && resultUpdateTrigger[1];
+            if (snapshot.val() !== null) {
+                const keys = Object.keys(snapshot.val());
+                const updateTriggerLatest = snapshot.val()[keys[0]].updateTrigger;
+                if (updateTriggerLatest === updateTrigger) {
+                    console.log('更新トリガーに変更がなかったためクローリング終了')
                     return null;
                 }
+            }
 
-                // 文字化け対策のDecode処理
-                const buf = Buffer.from(body, 'binary');
-                const retStr = iconv.decode(buf, constants.spots.iwa.charset);
+            // 透明度取得
+            const resultWaterClarity = retStr.match(constants.spots.iwa.regexp.waterClarity);
+            const waterClarity = resultWaterClarity && resultWaterClarity[1];
 
-                // 更新トリガーに変更がなければ処理終了
-                const resultUpdateTrigger = retStr.match(constants.spots.iwa.regexp.updateTrigger);
-                const updateTrigger = resultUpdateTrigger && resultUpdateTrigger[1];
-                if (snapshot.val() !== null) {
-                    const keys = Object.keys(snapshot.val());
-                    const updateTriggerLatest = snapshot.val()[keys[0]].updateTrigger;
-                    if (updateTriggerLatest === updateTrigger) {
-                        console.log('更新トリガーに変更がなかったためクローリング終了')
+            // DBへの書き込み
+            try {
+                const seaCondition = {
+                    waterClarity,
+                    createdAt: response.headers.date,
+                    updateTrigger,
+                    uri: constants.spots.iwa.uri,
+                }
+                admin.database().ref(constants.spots.iwa.dbPath).push(seaCondition);
+                console.log('DBへの書き込みに成功');
+
+                request({
+                    method: 'POST',
+                    uri: 'https://hooks.slack.com/services/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',
+                    encoding: null,
+                    json: {
+                        username: 'Ocean Alert',
+                        icon_emoji: ':ocean:',
+                        text: '岩の透明度：' + waterClarity + '\n' + constants.spots.iwa.uri,
+                    }
+                }, (e, r, b) => {
+                    if (e) {
+                        console.warn(e);
                         return null;
                     }
-                }
-
-                // 透明度取得
-                const resultWaterClarity = retStr.match(constants.spots.iwa.regexp.waterClarity);
-                const waterClarity = resultWaterClarity && resultWaterClarity[1];
-
-                // DBへの書き込み
-                try {
-                    const seaCondition = {
-                        waterClarity,
-                        createdAt: response.headers.date,
-                        updateTrigger,
-                        uri: constants.spots.iwa.uri,
-                    }
-                    admin.database().ref(constants.spots.iwa.dbPath).push(seaCondition);
-                    console.log('DBへの書き込みに成功');
+                    console.log(r);
+                    console.log(b);
                     return null;
-                } catch (error) {
-                    console.warn('DBへの書き込みに失敗');
-                    console.log(error);
-                    return null;
-                }
-            })
+                })
+                return null;
+            } catch (error) {
+                console.warn('DBへの書き込みに失敗');
+                console.log(error);
+                return null;
+            }
         })
-        .catch(function(error) {
-            console.log("Error deleting app:", error);
-        });
+    })
+    .catch(function(error) {
+        console.log("Error deleting app:", error);
+    });
     return null;
 })
